20565bf9d89dd9585ea397e66542f108da7761c5
Dmitry Antipov <antipovd2@biowulf.nih.gov>
2022-11-22 10:57:04 -0500

    wrapper now counts total errors

--- wrapper.py
+++ wrapper.py
@@ -10,3 +10,21 @@ for line in open(sys.argv[1], 'r'):
     arr = line.split()
     if len(arr)==2 and os.path.exists(arr[0]):
         os.system(f'python3 pipeline.py {arr[0]} {os.path.join(out_prefix, arr[1])} {sys.argv[2]}')
+#        print (arr[1])
+total_count = 0
+hiconly_count = 0
+datasets = 0
+for line in open(sys.argv[1], 'r'):
+    arr = line.split()
+    if len(arr)==2 and os.path.exists(arr[0]):
+        found = False
+        datasets += 1
+        for nline in open(os.path.join(out_prefix, arr[1], sys.argv[2])):
+            narr = nline.strip().split()
+            if len(narr) > 1 and narr[-1] == "errors":
+                if found:
+                    hiconly_count += int(narr[-2])
+                else:
+                    total_count += int(narr[-2])
+                found = True
+print (f'Total color switches: {total_count} using all triobinned, {hiconly_count} using only hic colored, totally in {datasets} datasets')
