01067085e7bb5ba5fca88c156a312cdc7203e687
Dmitry Antipov <dmitrij.antipov@gmail.com>
2022-12-29 22:09:48 -0500

    fxs

--- run_all_telomerefinder.py
+++ run_all_telomerefinder.py
@@ -4,6 +4,7 @@
 import sys
 import re
 import telomere_check
+import shutil
 import os
 
 submit_telomere = "/data/antipovd2/devel/vgp-assembly/pipeline/telomere/_submit_telomere.sh"
@@ -12,10 +13,12 @@ data_dir = "/data/antipovd2/data/"
 
 for dir in os.listdir(data_dir):
     workdir = os.path.join(data_dir, dir)
+    print (workdir)
     if os.path.isdir(workdir):
-        graph = os.path.join(workdir, "assembly_graph.gfa")
+        graph = os.path.join(workdir, "assembly_graph.fasta")
         if os.path.exists(graph):
+            os.chdir(workdir)
             run_line = f"{submit_telomere} {graph}"
             print(run_line)
-            #os.system(run_line)
-            shutil.move("assembly_graph", workdir)
+            os.system(run_line)
+#            shutil.move("assembly_graph", workdir)
