ad549eff6007cec223295b528ada876939398d69
Dmitry Antipov <dmitry.antipov@nih.gov>
2023-01-03 13:57:33 -0500

    uncomment main pipeline

--- check_all_t2t.py
+++ check_all_t2t.py
@@ -13,6 +13,7 @@ data_dir = "/Users/antipovd2/work/verkkohic/data/"
 for dir in os.listdir(data_dir):
     workdir = os.path.join(data_dir, dir)
     if os.path.isdir(workdir):
+        print (f'dataset: {dir}')
         t2t = os.path.join(workdir, "assembly_graph", "assembly_graph.windows.0.4.50kb.ends.bed")
         if os.path.exists(t2t):
             telomere_check.evaluate_telomers(os.path.join(workdir, "unitig-popped-unitig-normal-connected-tip.paths.tsv"), t2t)
