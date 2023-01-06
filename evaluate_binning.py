400a80d721364751ac55be8b6399b046e47c105d
Dmitry Antipov <dmitry.antipov@nih.gov>
2023-01-06 00:30:13 -0500

    better pre_rukki evaluation

--- evaluate_binning.py
+++ evaluate_binning.py
@@ -5,7 +5,8 @@ import os
 import random
 from os import listdir
 from os.path import isfile, join
-
+import networkx as nx
+import graph_functions
 def evaluate_set(contig_set, lengths, colors):
     m_len = 0
     p_len = 0
@@ -21,67 +22,102 @@ def evaluate_set(contig_set, lengths, colors):
                 elif colors[contig] == 'p':
                     p_len += l
     total_l = m_len + p_len + u_len
-    if total_l > 1000000:
-        print(f'{total_l}    {m_len / total_l:.3f}/{p_len / total_l:.3f}/{u_len / total_l:.3f}')
+    if total_l > 0:
+        print(f'Total & haplo lengths {total_l}    {m_len / total_l:.3f}/{p_len / total_l:.3f}/{u_len / total_l:.3f}')
         if m_len > 0 and p_len > 0:
             print('BAD')
+def evaluate_dataset(hic_file, gfa_file, trio_file, chromosomal_file):
+    # hi-c gfa(noseq) trio_colors
+    chrs = {}
+    if len(chromosomal_file) > 0:
+        for line in open(chromosomal_file):
+            arr = line.split()
+            contig = arr[0]
+            chr = arr[1]
+            if not (chr in chrs):
+                chrs[chr] = set()
+            chrs[chr].add(contig)
+    lengths = {}
+    for line in open(gfa_file, 'r'):
+        arr = line.split()
+        if arr[0] == "S":
+            lengths[arr[1]] = int(arr[3].split(':')[-1])
+    trio_colors = {}
+    for line in open(trio_file, 'r'):
+        arr = line.split()
+        name = arr[0]
+        color = "a"
+        if arr[4] == "#8888FF":
+            color = "m"
+        elif arr[4] == "#FF8888":
+            color = "p"
+        trio_colors[name] = color
+    hic_colors = {}
+    for line in open(hic_file, 'r'):
+        arr = line.split()
+        name = arr[0]
+        color = "a"
+        if arr[4] == "#8888FF":
+            color = "m"
+        elif arr[4] == "#FF8888":
+            color = "p"
+        hic_colors[name] = color
+    if len(chrs) == 0:
+        chrs['ALL'] = set()
+        for name in trio_colors.keys():
+            chrs['ALL'].add(name)
+    G = nx.Graph()
+    graph_functions.load_indirect_graph(gfa_file, G)
+    graph_functions.remove_large_tangles(G, 200000, 100)
+
+    for c in sorted(nx.connected_components(G), key=len, reverse=True):
+        #    print("Connected component with %d nodes is: %s" % (len(c), c))
+        mset = set()
+        pset = set()
+        count = 0
+        for e in c:
+            if e in hic_colors:
+                if hic_colors[e] == 'p':
+                    pset.add(e)
+                else:
+                    mset.add(e)
+                count += 1
+        if len(mset) + len (pset) > 1:
+            print(f"Evaluating component of {len(mset)} and {len(pset)} edges of 1/2 haplotypes")
+            evaluate_set(mset, lengths, trio_colors)
+            evaluate_set(pset, lengths, trio_colors)
+'''
+    for line in open(comp_file, 'r'):
+        if line.split()[0] != "RES":
+            continue
+        print()
+        print(line.strip())
+        for lines in line.split('}, {'):
+            arr = lines.split('\'')
+            cont_set = set(arr)
+            nonempty = 0
+            for chr in chrs:
+                chr_comp = chrs[chr].intersection(cont_set)
+                if len(chr_comp) > 0:
+                    nonempty += 1
+            for chr in chrs:
+                chr_comp = chrs[chr].intersection(cont_set)
+                if len(chr_comp) > 0:
+                    print(f'Non empty component on chr {chr}')
+                    if nonempty == 1:
+                        evaluate_set(cont_set, lengths, colors)
+                    else:
+                        print(chr_comp)
+                        evaluate_set(chr_comp, lengths, colors)
+    #    exit()
+'''
 
 if len(sys.argv) < 4:
     print(f'Usage: {sys.argv[0]} <hi-c phasing results> <graph.gfa> <trio coloring> [mashmap based chromosome assignment]')
     exit()
-#hi-c gfa(noseq) trio_colors
-comp_file = sys.argv[1]
-gfa_file = sys.argv[2]
-trio_file = sys.argv[3]
-chrs = {}
-if len(sys.argv) > 4:
-    chromosomal_file = sys.argv[4]
-    for line in open (chromosomal_file):
-        arr = line.split()
-        contig = arr[0]
-        chr = arr[1]
-        if not (chr in chrs):
-            chrs[chr] = set()
-        chrs[chr].add(contig)
-lengths = {}
-for line in open(gfa_file, 'r'):
-    arr = line.split()
-    if arr[0] == "S":
-        lengths[arr[1]] = int(arr[3].split(':')[-1])
-colors = {}
-for line in open(trio_file, 'r'):
-    arr = line.split()
-    name = arr[0]
-    color = "a"
-    if arr[4] == "#8888FF":
-        color = "m"
-    elif arr[4] == "#FF8888":
-        color = "p"
-    colors[name] = color
-if len(chrs) == 0:
-    chrs['ALL'] = set()
-    for name in colors.keys():
-        chrs['ALL'].add(name)
-for line in open (comp_file, 'r'):
-    if line.split()[0] != "RES":
-        continue
-    print()
-    print(line.strip())
-    for lines in line.split('}, {'):
-        arr = lines.split('\'')
-        cont_set = set(arr)
-        nonempty = 0
-        for chr in chrs:
-            chr_comp = chrs[chr].intersection(cont_set)
-            if len (chr_comp) > 0:
-                nonempty+=1
-        for chr in chrs:
-            chr_comp = chrs[chr].intersection(cont_set)
-            if len (chr_comp) > 0:
-                print (f'Non empty component on chr {chr}')
-                if nonempty == 1:
-                    evaluate_set(cont_set, lengths, colors)
-                else:
-                    print (chr_comp)
-                    evaluate_set(chr_comp, lengths, colors)
-#    exit()
+
+if __name__ == "__main__":
+    chromosomal_file = ""
+    if len (sys.argv) == 5:
+        chromosomal_file = sys.argv[4]
+    evaluate_dataset(sys.argv[1], sys.argv[2], sys.argv[3], chromosomal_file)
